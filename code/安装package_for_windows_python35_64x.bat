db81bdb5af42ca91ba5631763622489aa2bd8fb1
Jiaxun Song <jiaxunsong@Jiaxuns-MacBook-Pro.local>
2018-08-09 21:31:06 -0700

    产品分支

--- code/安装package_for_windows_python35_64x.bat
+++
@@ -1,6 +0,0 @@
-@echo 正在安装package。。。
-@echo off
-%USERPROFILE%\AppData\Local\Programs\Python\Python36\python -m pip install --upgrade pip
-%USERPROFILE%\AppData\Local\Programs\Python\Python36\Scripts\pip install -r requirements.txt
-echo.安装成功！
-pause
