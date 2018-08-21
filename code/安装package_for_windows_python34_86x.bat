18bee677fa2a987b92ef1b78d83db4cf5a4f57cc
jiaxunsongucb <jiaxun.song@outlook.com>
2018-08-21 12:59:59 +0800

    keep 86x only

--- code/安装package_for_windows_python34_86x.bat
+++ code/安装package_for_windows_python34_86x.bat
@@ -1,9 +1,8 @@
 @echo 正在安装package。。。
 @echo off
-C:\Python34_86x\python -m pip install --upgrade pip
-C:\Python34_86x\Scripts\pip3.4 install -r requirements1.txt
-C:\Python34_86x\Scripts\pip3.4 install -r requirements2.txt
-echo from PyInstaller.utils.hooks import collect_submodules > C:\Python34_86x\Lib\site-packages\PyInstaller\hooks\hook-pandas.py
-echo hiddenimports = collect_submodules('pandas._libs') >> C:\Python34_86x\Lib\site-packages\PyInstaller\hooks\hook-pandas.py
+C:\Python34\python -m pip install --upgrade pip
+for /F %%A in (requirements.txt) do C:\Python34\Scripts\pip install %%A
+echo from PyInstaller.utils.hooks import collect_submodules > C:\Python34\Lib\site-packages\PyInstaller\hooks\hook-pandas.py
+echo hiddenimports = collect_submodules('pandas._libs') >> C:\Python34\Lib\site-packages\PyInstaller\hooks\hook-pandas.py
 echo.安装成功！
 pause
--- code/生成exe文件_python34_64x.bat
+++
@@ -1,5 +0,0 @@
-@echo 生成exe文件。
-@echo off
-C:\Python34\Scripts\pyinstaller.exe "%cd%\library_system64x.spec"
-echo.生成成功！
-pause
